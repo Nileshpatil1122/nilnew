91f6fe9dcf7f4451404d24070480c351ee1596e5
Nileshpatil1122 <129361420+Nileshpatil1122@users.noreply.github.com>
2023-05-23 16:18:18 +0530

    Add files via upload

---
+++ Precentage.java
@@ -0,0 +1,21 @@
+import java.util.Scanner ;
+class Natural
+{
+public static void main(String)
+{
+Scanner sc=new Scanner(System.in) ;
+System.out.println("ENTER THE NUMBER");
+int num=sc.nextInt();
+
+if(number>0)
+{
+System.out.println("THE NUMBER IS NATURAL");
+}
+
+
+else
+{
+System.out.println("THE NUMBER IS NOT NATURAL");
+}
+}
+}
